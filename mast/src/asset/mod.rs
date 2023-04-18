6eaf85033b01c4fddd229982faa28799fa9aa7fe
SabrinaJewson <sejewson@gmail.com>
2023-04-18 09:50:01 +0100

    Write more docs

--- mast/src/asset/mod.rs
+++ mast/src/asset/mod.rs
@@ -55,18 +55,44 @@ mod tracked {
     }
 
     impl<T> Tracked<T> {
+        /// Construct a value that is always the same.
+        ///
+        /// # Example
+        ///
+        /// ```
+        /// # use mast::asset::Delta;
+        /// # use mast::asset::Tracked;
+        /// assert_eq!(Tracked::constant(37).delta, Delta::Same);
+        /// ```
         #[must_use]
         pub const fn constant(value: T) -> Self {
             Delta::Same.track(value)
         }
+        /// Check whether `self.delta == Delta::Same`.
+        ///
+        /// # Example
+        ///
+        /// ```
+        /// # use mast::asset::Tracked;
+        /// assert!(Tracked::constant(37).is_same());
+        /// ```
         #[must_use]
         pub const fn is_same(&self) -> bool {
             matches!(self.delta, Delta::Same)
         }
+        /// Check whether `self.delta == Delta::Modified`.
+        ///
+        /// # Example
+        ///
+        /// ```
+        /// # use mast::asset::Tracked;
+        /// assert!(!Tracked::constant(37).is_modified());
+        /// ```
         #[must_use]
         pub const fn is_modified(&self) -> bool {
             matches!(self.delta, Delta::Modified)
         }
+        /// Convert a `&Tracked<T>` to a `Tracked<&T>`.
         #[must_use]
         pub const fn as_ref(&self) -> Tracked<&T> {
             Tracked {
@@ -74,6 +100,7 @@ mod tracked {
                 delta: self.delta,
             }
         }
+        /// Convert a `&mut Tracked<T>` to a `Tracked<&mut T>`.
         #[must_use]
         pub fn as_mut(&mut self) -> Tracked<&mut T> {
             Tracked {
@@ -97,6 +124,15 @@ pub enum Delta {
 }
 
 impl Delta {
+    /// Compare a previous and current value and return the appropriate `Delta`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use mast::asset::Delta;
+    /// assert_eq!(Delta::cmp(&37, &37), Delta::Same);
+    /// assert_eq!(Delta::cmp(&17, &37), Delta::Modified);
+    /// ```
     #[must_use]
     pub fn cmp<T: PartialEq<U>, U>(prev: &T, current: &U) -> Self {
         if prev == current {
@@ -106,6 +142,18 @@ impl Delta {
         }
     }
 
+    /// Combine two deltas such that the overall result is considered modified
+    /// if either delta is modified.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use mast::asset::Delta;
+    /// assert_eq!(Delta::Same.or(Delta::Same), Delta::Same);
+    /// assert_eq!(Delta::Same.or(Delta::Modified), Delta::Modified);
+    /// assert_eq!(Delta::Modified.or(Delta::Same), Delta::Modified);
+    /// assert_eq!(Delta::Modified.or(Delta::Modified), Delta::Modified);
+    /// ```
     #[must_use]
     pub const fn or(self, other: Self) -> Self {
         match self {
@@ -114,6 +162,21 @@ impl Delta {
         }
     }
 
+    /// Lazily combine two deltas such that the overall result is considered modified
+    /// if either delta is modified.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use mast::asset::Delta;
+    /// fn compute_delta() -> Delta { Delta::Modified }
+    ///
+    /// // `compute_delta` is not called
+    /// assert_eq!(Delta::Modified.or_else(|| compute_delta()), Delta::Modified);
+    ///
+    /// // `compute_delta` *is* called
+    /// assert_eq!(Delta::Same.or_else(|| compute_delta()), Delta::Modified);
+    /// ```
     #[must_use]
     pub fn or_else<F: FnOnce() -> Delta>(self, f: F) -> Self {
         match self {
@@ -122,23 +185,22 @@ impl Delta {
         }
     }
 
+    /// Construct a [`Tracked`] using this value as its delta.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use mast::asset::Delta;
+    /// # use mast::asset::Tracked;
+    /// fn number() -> Tracked<u32> {
+    ///     Delta::Same.track(37)
+    /// }
+    /// assert!(number().is_same());
+    /// ```
     #[must_use]
     pub const fn track<T>(self, value: T) -> Tracked<T> {
         Tracked { value, delta: self }
     }
-}
-
-/// Trait for the subset of [`Asset`]s whose regeneration logic just
-/// calculates the new etag and compares it with the old one.
-/// Used to implement [`Asset::hash_etag`].
-///
-/// Examples of assets that do _not_ implement this trait
-/// include assets representing content
-/// downloaded from the Internet,
-/// because obtaining the etag is done at the same time as the generation step.
-pub trait EtagCmp: Asset {
-    /// Calculate the current etag.
-    fn etag(&self) -> Self::Etag;
 }
 
 use crate::etag::Etag;
